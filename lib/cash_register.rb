8026dd58e7a76fb0f8221fc669fab8da4359230a
Jacqueline Chenault <adigitalnative@gmail.com>
2018-09-18 21:07:55 -0400

    WIP

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,32 @@
+require 'pry'
 
+class CashRegister
+
+  attr_accessor :discount, :total, :items
+
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(name, price, quantity=1)
+    @total += price * quantity
+    quantity.times do
+      @items << name
+    end
+  end
+
+  def apply_discount
+    if discount > 0
+      # Convert discount value to a float
+      discount_as_float = discount.to_f * 0.01
+      @total -= @total * discount_as_float
+
+      return "After the discount, the total comes to $#{@total.to_i}."
+    else
+      return "There is no discount to apply."
+    end
+  end
+
+end
